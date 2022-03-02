124c9d19c6ecff46631ed7d39b2d52e9fce2255d
IGarcia2000 <aicragnavi2.0@outlook.com>
2022-03-02 11:15:41 -0600

    Se agrego la carpeta login

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var apli = http.createServer(function(request, response){
         'Content-type':'text/html'
     })
     response.write("<h1>Hola mundo</h1>");
+    response.write("<h2>Esto es una prueba de git</h2>")
     response.end();
 });
 
